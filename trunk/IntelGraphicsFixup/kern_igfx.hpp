74ada764c91115f99572f117764a1c94981c6917
lvs1974 <lvs1974@83e91cb9-a62b-4301-b08b-4f2bb4998a18>
2017-12-16 22:44:04 +0000

    Version 1.2.1: New boot  logo fix via restoring video memory content (frame buffer reset is performed only as fallback)

--- trunk/IntelGraphicsFixup/kern_igfx.hpp
+++ trunk/IntelGraphicsFixup/kern_igfx.hpp
@@ -14,8 +14,15 @@ class IGFX {
 public:
 	bool init();
 	void deinit();
-	
+
 private:
+	/**
+	 *  Obtain necessary symbols from the kernel
+	 *
+	 *  @param patcher KernelPatcher instance
+	 */
+	void processKernel(KernelPatcher &patcher);
+
 	/**
 	 *  Patch kext if needed and prepare other patches
 	 *
@@ -46,8 +53,7 @@ private:
 	 *  computeLaneCount type
 	 */
 	using t_compute_lane_count = bool (*)(void *, void *, unsigned int, int, int *);
-		
-	
+
 	/**
 	 *  Hooked methods / callbacks
 	 */
@@ -63,9 +69,57 @@ private:
 	t_compute_lane_count orgComputeLaneCount {nullptr};
 		
 	/**
-	 *  external global variables
+	 *  External global variables
 	 */
 	uint8_t *gIOFBVerboseBootPtr {nullptr};
+
+	enum FramebufferFixMode {
+		FBDEFAULT  = 0,
+		FBRESET    = 1,
+		FBCOPY     = 2
+	};
+
+	/**
+	 *  Framebuffer distortion fix mode
+	 */
+	uint32_t resetFramebuffer {FBDEFAULT};
+
+	/**
+	 *  Console info structure, taken from osfmk/console/video_console.h
+	 *  Last updated from XNU 4570.1.46.
+	 */
+	struct vc_info {
+		unsigned int   v_height;        /* pixels */
+		unsigned int   v_width;         /* pixels */
+		unsigned int   v_depth;
+		unsigned int   v_rowbytes;
+		unsigned long  v_baseaddr;
+		unsigned int   v_type;
+		char           v_name[32];
+		uint64_t       v_physaddr;
+		unsigned int   v_rows;          /* characters */
+		unsigned int   v_columns;       /* characters */
+		unsigned int   v_rowscanbytes;  /* Actualy number of bytes used for display per row*/
+		unsigned int   v_scale;
+		unsigned int   v_rotate;
+		unsigned int   v_reserved[3];
+	};
+
+
+	/**
+	 *  vinfo presence status
+	 */
+	bool gotInfo {false};
+
+	/**
+	 *  Loaded vinfo
+	 */
+	vc_info vinfo {};
+
+	/**
+	 *  Console buffer backcopy
+	 */
+	uint8_t *consoleBuffer {nullptr};
 	
 	/**
 	 *  Current progress mask
