d0e27c5ea2a199961782d8dfe3e253b222892c3e
vit9696 <vit9696@users.noreply.github.com>
2018-02-24 01:31:47 +0000

    Initial code to attempt to load Apple scheduler, currently fails with an unknown mia code.

--- IntelGraphicsFixup/kern_guc.hpp
+++ IntelGraphicsFixup/kern_guc.hpp
@@ -11,6 +11,7 @@
 #include <libkern/libkern.h>
 
 static constexpr size_t GuCFirmwareSignatureSize = 256;
+static constexpr size_t HuCFirmwareSignatureSize = 256;
 
 extern const uint8_t *GuCFirmwareSKL;
 extern const uint8_t *GuCFirmwareSKLSignature;
@@ -19,5 +20,13 @@ extern const size_t GuCFirmwareSKLSize;
 extern const uint8_t *GuCFirmwareKBL;
 extern const uint8_t *GuCFirmwareKBLSignature;
 extern const size_t GuCFirmwareKBLSize;
+
+extern const uint8_t *HuCFirmwareSKL;
+extern const uint8_t *HuCFirmwareSKLSignature;
+extern const size_t HuCFirmwareSKLSize;
+
+extern const uint8_t *HuCFirmwareKBL;
+extern const uint8_t *HuCFirmwareKBLSignature;
+extern const size_t HuCFirmwareKBLSize;
 
 #endif /* kern_guc_h */
